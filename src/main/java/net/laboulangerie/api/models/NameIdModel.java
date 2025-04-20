a48c8bee5171323470c8666d6ca223559011f01f
Matteo Planchet <matteo.planchet@gmail.com>
2025-04-20 14:40:48 +0200

    fix protected fields

--- src/main/java/net/laboulangerie/api/models/NameIdModel.java
+++ src/main/java/net/laboulangerie/api/models/NameIdModel.java
@@ -3,8 +3,8 @@ package net.laboulangerie.api.models;
 import java.io.Serializable;
 
 public class NameIdModel implements Serializable {
-    private String name = null;
-    private String id = null;
+    protected String name = null;
+    protected String id = null;
 
     public NameIdModel(String name, String id) {
         this.name = name;
